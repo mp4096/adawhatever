dea8e588ef97468cbaed8e98d873bd12e28bdfb6
mp4096 <mikhail.pak@tum.de>
2015-12-26 23:26:08 +0100

    Fixed the sparse benchmark
    Before: `Qa` was indefinite => there was no minimum
    Now: Each `Qa{i}` is positive definite with a very large condition number
    
    Also added an averaged gradient benchmark

--- +Tests/quadSumSparse500D.m
+++ +Tests/quadSumSparse500D.m
@@ -22,34 +22,69 @@ objFun = @(x) 0.5*([x', 1]*QaAvg*[x; 1]);
 grad = @(x) ([x; 1]'*QaAvg(:, 1 : 1 : end - 1))';
 gradStoch = @(i, x) ([x; 1]'*Qa{i}(:, 1 : 1 : end - 1))';
 
-%% Perform optimisation
-
-% Adamax has problems because of missing gradients: 0/0 = NaN
+%% Perform optimisation -- Purely stochastic gradient
 
 x0 = ones(nDecVar, 1);
-nIter = 500;
+nIter = 1000;
 idxSG = randi(nQa, 1, nIter);
 
 solvers = {'Adam', 'Adamax', 'AdaGrad', 'AdaGradDecay', 'VanillaSGD'};
 
-xMat.Adam = Adam(gradStoch, x0, 1e-1, idxSG, nIter, 0.8, 0.999);
+xMat.Adam = Adam(gradStoch, x0, 1e-1, idxSG, nIter, 0.9, 0.999);
 xMat.Adamax = Adamax(gradStoch, x0, 1e-1, idxSG, nIter, 0.9, 0.999);
 xMat.AdaGrad = AdaGrad(gradStoch, x0, 1e-1, idxSG, nIter);
 xMat.AdaGradDecay = AdaGradDecay(gradStoch, x0, 1e-1, idxSG, nIter, 0.9);
-xMat.VanillaSGD = VanillaSGD(gradStoch, x0, 1e-6, idxSG, nIter);
+xMat.VanillaSGD = VanillaSGD(gradStoch, x0, 1e-1, idxSG, nIter);
 
 for i = 1 : 1 : length(solvers)
     objFunMat.(solvers{i}) = ...
         cellfun(objFun, num2cell(xMat.(solvers{i}), 1));
 end
 
-%% Plot results -- Convergence plot
+%% Plot results -- Convergence plot -- Purely stochastic gradient
 
-figConvergence = figure( ...
-    'Name', 'Convergence behaviour of different solvers');
+figure
+ax(1) = subplot(1, 2, 1);
 for i = 1 : 1 : length(solvers)
     plot(objFunMat.(solvers{i}));
     hold on
 end
 hold off
+grid on
 legend(solvers);
+title(['Convergence behaviour of different solvers, ', ...
+    'stochastic gradient only'])
+
+%% Perform optimisation -- Averaged stochastic gradient
+
+% Average over 10 random gradients
+idxSG = randi(nQa, 10, nIter);
+
+avgSG = @(idx, x) AvgGrad(gradStoch, idx, x);
+
+xMat.Adam = Adam(avgSG, x0, 1e-1, idxSG, nIter, 0.9, 0.999);
+xMat.Adamax = Adamax(avgSG, x0, 1e-1, idxSG, nIter, 0.9, 0.999);
+xMat.AdaGrad = AdaGrad(avgSG, x0, 1e-1, idxSG, nIter);
+xMat.AdaGradDecay = AdaGradDecay(avgSG, x0, 1e-1, idxSG, nIter, 0.9);
+xMat.VanillaSGD = VanillaSGD(avgSG, x0, 1e-1, idxSG, nIter);
+
+for i = 1 : 1 : length(solvers)
+    objFunMat.(solvers{i}) = ...
+        cellfun(objFun, num2cell(xMat.(solvers{i}), 1));
+end
+
+%% Plot results -- Convergence plot -- Averaged stochastic gradient
+
+ax(2) = subplot(1, 2, 2);
+for i = 1 : 1 : length(solvers)
+    plot(objFunMat.(solvers{i}));
+    hold on
+end
+hold off
+grid on
+legend(solvers);
+title(['Convergence behaviour of different solvers, ', ...
+    'averaged stochastic gradient']);
+
+linkaxes(ax, 'xy');
+xlim([1, nIter]);
